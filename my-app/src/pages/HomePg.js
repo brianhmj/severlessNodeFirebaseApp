a7c2438efded26754fd420c6e2beac89723654c4
Brian Jung <65934595+brianhmj@users.noreply.github.com>
2022-09-15 17:58:52 -0400

    Add files via upload

---
+++ my-app/src/pages/HomePg.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function HomePg() {
+   return (
+     <div className="App">
+         This is the home page
+     </div>
+   );
+ }
+ export default HomePg;
---
+++ my-app/src/pages/PostPg.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function PostPg() {
+   return (
+     <div className="App">
+         This is the post page
+     </div>
+   );
+ }
+ export default PostPg;
